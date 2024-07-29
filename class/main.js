317bc779324cc6d275a41d95ae98ab809244453f
nabeela77 <149621875+nabeela77@users.noreply.github.com>
2024-07-29 16:23:41 -0500

    created main.js

---
+++ class/main.js
@@ -0,0 +1,16 @@
+// main.js
+// camelCasing - camel casing (main casing we'll be using in JavaScript)
+// kebob-casing - kebob casing
+// snake_casing - snake casing
+
+// JS/Python (interpreted language, interpreter program) -> interpreter executes the JS code line by line
+// garbage collection: for memory management
+
+// Java/C# (compiled / interpeted language) -> bytecode -> ported to differents computer (Java VM - Java only) -> executes intermediate bytecode or compiles into machine code -> runs it
+// garbage collection: for memory management
+
+// C/C++ (compiled) -> machine language (0,1) (x86) -> cpu executes/runs it
+// No garbage collection - manual memory management
+// Low energy cost / highly efficient / fast runtime / lower memory / low security and vulnerability
+
+// ML framework runs on Python (Tensorflow, Pytorth, Scilearn, Numpy, Pandas)
