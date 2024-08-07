085861f42a49089f39533f3d025fd89cf7912a31
nabeela77 <149621875+nabeela77@users.noreply.github.com>
2024-08-07 13:18:35 -0700

    comparing primitives and functions

--- class/array.js
+++ class/array.js
@@ -1,11 +1,54 @@
 // create an array
-const arrA = [];
-console.log(arrA);
+// const arrA = [];
+// console.log(arrA);
 
 // update an array
-arrA[1] = 999;
-arrA[2] = 999.5;
-arrA.push(1000);
+// arrA[1] = 999;
+// arrA[2] = 999.5;
+// arrA.push(1000, 1002, 1003);
+// arrA.push([1004, 1005, 1006]);
+// arrA[10] = {};
+// arrA[1] = 999.3
+// arrA[10] = {};creates object with {} index7,8,9 are empty
 
-console.log(arrA);
-// console.log(arrA[0]);
+
+//  console.log(arrA[0]);
+// console.log(arrA);
+// console.log(arrA.length);
+
+// const fruits = [];
+// console.log(fruits);
+
+// fruits[1] = "apple"
+// fruits[2] = "banana"
+// fruits[9] = "strawberry"
+// fruits.push(["kiwi", "raspberry"])
+// fruits[1] = "blueberry"
+// console.log(fruits.length)
+
+// console.log(fruits)
+
+//  const itemsRemoved = fruits.pop();
+// console.log(fruits);
+// item = fruits.unshift("ube")
+// const itm = fruits.shift()
+// console.log(fruits)
+// console.log(itm)
+
+// function loopPrint(array){
+
+// }
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function sumList(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        sum = sum + element;
+    }
+    console.log(sum)
+}
+
+sumList(nums)
+sumList([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
--- class/compare.js
+++ class/compare.js
@@ -15,3 +15,37 @@ console.log(8 == 7); // false
 // other primitives
 console.log(0 === false); // false
 console.log(0 == false); // true
+
+// compare primitives
+let a = 5;
+let b = a; // 5 (b is copied-by-value ONCE)
+let c = 5;
+
+// console.log("a === c", a === c); // true
+// console.log("a === b", a === b); // true
+// console.log("b === c", b === c); // true
+
+// a = 10;
+// console.log("a === c", a === c); // 10 === 5 false -> false
+// console.log("a === b", a === b); // 10 === 10 true -> 10 === 5 false (b is copied-by-value ONCE)
+// console.log("b === c", b === c); // 10 === 5 false -> 5 === 5 true
+
+// comparing arrays (copy-by-value, but copying the reference memory address -> copy-by-reference value)
+const arrA = [10]; // 'ab10ef'
+const arrB = arrA; // 'ab10ef'
+const arrC = [10]; // 'ec45ba'
+const arrD = [10]; // 'bae65f'
+
+console.log("arrA === arrB", arrA === arrB); // 'ab10ef' === 'ab10ef' true
+console.log("arrA === arrC", arrA === arrC); // 'ab10ef' === 'ec45ba' false
+console.log("arrC === arrD", arrC === arrD); // 'ec45ba' === 'bae65f' false
+
+arrA[0] = 100;
+console.log("arrA", arrA); // [100]
+console.log("arrB", arrB); // [100]
+console.log("arrC", arrC); // [10]
+console.log("arrA === arrB", arrA === arrB); // true, true, true
+
+arrB[0] = 200
+console.log("arrA", arrA); //[200]as it has same address as arrB
+console.log("arrB", arrB); //[200]as it has same address as arrA
