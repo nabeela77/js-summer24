7aa28195fbc89bc2f06e3bcb636cdc8550afc50a
nabeela77 <149621875+nabeela77@users.noreply.github.com>
2024-10-18 13:39:49 -0700

    added hasAccess to pages

--- react-app/src/components/Layout/AccessControl.jsx
+++ react-app/src/components/Layout/AccessControl.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
 
 // role : "student", "teacher", "admin", "super-admin"
 // requiredRoles: ["admin", "super-admin"]
 
 const AccessControl = ({ requiredRoles, role, children }) => {
-  const hasAccess = () => {};
+  const hasAccess = () => {
+    if (!requiredRoles || requiredRoles.length === 0) {
+      //required roles falsy means that their is no need for required role
+      return true;
+    }
+    return requiredRoles.includes(role);
+  };
+  return hasAccess() ? children : <Navigate to="/unauthorized" replace />;
 };
 
 export default AccessControl;
